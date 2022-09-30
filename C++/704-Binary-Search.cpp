87876f5ed39e8e5638857fd997a57d234aec5b4d
neroAzsy12 <azael.zamora12@gmail.com>
2022-09-30 16:16:07 -0700

    Add files via upload

---
+++ C++/704-Binary-Search.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+    /**
+     * Binary Search Algorithm
+     * TC: O(log n), where n is the number of elements in nums
+     * SC: O(1), no extra space is allocated
+     */
+    int search(vector<int>& nums, int target) {
+      int left = 0;
+      int right = nums.size() - 1;
+
+      while (left <= right) {
+        int middle = (left + right) / 2;  // int middle = left + (right - left) / 2;
+
+        // check if target is located in the middle
+        if (nums[middle] == target) return middle;
+
+        // if target is greater, ingnore left half
+        else if (nums[middle] < target) left = middle + 1;
+        
+        // if target is smaller, ignore right half
+        else right = middle - 1;
+      }
+
+      // if we reach here, then the target element was not present
+      return -1;
+    }
+};
