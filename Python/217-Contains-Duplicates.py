db8b4de0217e2006a19f35a3c7375bc969c407b3
neroAzsy12 <azael.zamora12@gmail.com>
2022-09-30 23:50:28 -0700

    Add files via upload

---
+++ Python/217-Contains-Duplicates.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+  '''
+  TC: O(n), where n is the number of elements in nums
+  SC: O(n), since we need to allocate a set that can be nums.size()
+  '''
+  def containsDuplicate(self, nums: List[int]) -> bool:
+    unique = set()  # our hashset
+
+    for num in nums:
+      if num in unique:
+        return True
+      unique.add(num)
+    
+    return False
